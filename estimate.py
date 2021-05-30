898f04db979dcc790b0134e050bdb0b437ebf9e5
Goudam <goudamunni.01@gmail.com>
2021-05-30 22:13:52 +0530

    Estimating pi using Wallis formula and Monte carlo simulation

--- estimate.py
+++ estimate.py
@@ -1,5 +1,29 @@
 import math
 import unittest
+import random
+
+def wallis(n):
+    #First I am assigning pi value as 1
+    pi_value=1    
+    for i in range (1,n+1):
+       #Calculating pi using the Wallis formula 
+       pi_value = pi_value * ((4*i*i) / (4*i*i - 1))
+    return 2*(pi_value)
+
+def monte_carlo(n):
+   #First I am assigning points inside the circle as 0
+   p_circle = 0   
+   for i in range(n):
+        x,y=random.random(),random.random()
+        #Calculating the distance between the random point (x,y) and the centre of the cirlce
+        d = (((x-0.5)**2)+((y-0.5)**2))**0.5
+        if d <= 0.5:
+            p_circle = p_circle + 1
+   #Calculating the pi value using the monte carlo simulation           
+   pi_value = (p_circle/n)*4 
+   return pi_value
+   
+
 
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
